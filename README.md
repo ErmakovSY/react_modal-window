d100bf7b37cd460dc0f4e179451362eee315839b
ErmakovSY <erm.sergey1989@gmail.com>
2017-11-28 17:21:06 +0300

    fixed some bugs

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-# Modal Window with React.js
+# Modal window with React.js
 
-Using styled-components and material-ui
+Using styled-components
 
-### Install:
-```
-npm install --save react-modal-alert
-```
+### Manual:
+1. npm i
+2. npm start
 
 ### Usage:
 ```javascript
-import Modal from 'react-modal-alert';
+import Modal from './components/Modal';
 
 ReactDOM.render((
   <Modal 
@@ -22,22 +21,22 @@ ReactDOM.render((
 );
 ```
 
-### List of properties:
-|  №  |      Property     | Required |  Type   | Description                                                 |
-|:---:|:------------------|:--------:|:-------:|:------------------------------------------------------------|
-|  1  | title             | true     | string  | Title of modal window                                       |
-|  2  | text              | true     | string  | Text message of modal window                                |
-|  3  | type              | true     | string  | Type of message ('success', 'notify', 'warning' or 'error') |
-|  4  | visible           | true     | boolean | Parameter for control showing of modal window               |
-|  5  | overlayStyle      | false    | object  | Style of overlay layout                                     |
-|  6  | wrapperStyle      | false    | object  | Style of window wrapper                                     |
-|  7  | headerStyle       | false    | object  | Style of window header                                      |
-|  8  | contentStyle      | false    | object  | Style of message section                                    |
-|  9  | footerStyle       | false    | object  | Style of window footer                                      |
-|  10 | titleStyle        | false    | object  | Style of window title                                       |
-|  11 | closeButtonStyle  | false    | object  | Style of window close button                                |
-|  12 | submitButtonStyle | false    | object  | Style of window submit button                               |
-|  13 | contentTextStyle  | false    | object  | Style of window message                                     |
+### List of properties
+|  №  |      Property     | Require |  Type   | Description                                                 |
+|:---:|:------------------|:-------:|:-------:|:------------------------------------------------------------|
+|  1  | title             | true    | string  | Title of modal window                                       |
+|  2  | text              | true    | string  | Text message of modal window                                |
+|  3  | type              | true    | string  | Type of message ('success', 'notify', 'warning' or 'error') |
+|  4  | visible           | true    | boolean | Parameter for control showing of modal window               |
+|  5  | overlayStyle      | false   | object  | Style of overlay layout                                     |
+|  6  | wrapperStyle      | false   | object  | Style of window wrapper                                     |
+|  7  | headerStyle       | false   | object  | Style of window header                                      |
+|  8  | contentStyle      | false   | object  | Style of message section                                    |
+|  9  | footerStyle       | false   | object  | Style of window footer                                      |
+|  10 | titleStyle        | false   | object  | Style of window title                                       |
+|  11 | closeButtonStyle  | false   | object  | Style of window close button                                |
+|  12 | submitButtonStyle | false   | object  | Style of window submit button                               |
+|  13 | contentTextStyle  | false   | object  | Style of window message                                     |
 
 ### License:
 MIT License.
--- src/index.js
+++ src/index.js
@@ -1,116 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import FontAwesome from 'react-fontawesome';
-import { Wrapper, Header, Content, Footer, Title, Text, Icon, Overlay, Close, Button } from './components/styledComponents.js';
+import ReactDOM from 'react-dom';
+import Modal from './components/Modal';
+import registerServiceWorker from './registerServiceWorker';
 
-export default class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false
-    }
-  }
-
-  componentWillMount = () => {
-    this.setState({
-      visible: this.props.visible
-    })
-  }
-
-  componentWillReceiveProps = (nextProps) => {
-    this.setState({
-      visible: nextProps.visible
-    })
-  }
-
-  closeModal = () => {
-    this.setState({
-      visible: false
-    })
-  }
-
-  getIcon = (type) => {
-    switch(type) {
-      case "success": 
-        return {
-          name: "check", 
-          color: "#008500"
-        };
-      case "notify": 
-        return {
-          name: "commenting-o", 
-          color: "#E6FC3F"
-        };
-      case "warning": 
-        return {
-          name: "exclamation-triangle", 
-          color: "#A66F00"
-        };
-      case "error": 
-        return {
-          name: "times", 
-          color: "#BF5430"
-        };
-      default: 
-        return {
-          name: "exclamation", 
-          color: "#FFBF40"
-        };
-    }
-  }
-
-  render = () => {
-    const { title, text, type, overlayStyle, wrapperStyle, headerStyle, contentStyle, footerStyle, titleStyle, closeButtonStyle, submitButtonStyle, contentTextStyle } = this.props;
-    const { visible } = this.state;
-    const icon = this.getIcon(type);
-    return (
-      <Overlay 
-        hidden={!visible}
-        style={overlayStyle}
-      >
-        <Wrapper style={wrapperStyle}>
-          <Header style={headerStyle}>
-            <Title style={titleStyle}>
-              {title}
-            </Title>
-            <Close 
-              onClick={this.closeModal}
-              style={closeButtonStyle}
-            >
-              <FontAwesome name="times" />
-            </Close>
-          </Header>
-          <Content style={contentStyle}>
-            <Icon iconColor={icon.color}>
-              <FontAwesome name={icon.name} />
-            </Icon>
-            <Text style={contentTextStyle}>
-              {text}
-            </Text>
-          </Content>
-          <Footer style={footerStyle}>
-            <Button style={submitButtonStyle}>
-              <span>OK</span>
-            </Button>
-          </Footer>
-        </Wrapper>
-      </Overlay>
-    )
-  }
-}
-
-Modal.propTypes = {
-  title: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['success', 'notify', 'warning', 'error']).isRequired,
-  visible: PropTypes.bool.isRequired,
-  overlayStyle: PropTypes.object,
-  wrapperStyle: PropTypes.object,
-  headerStyle: PropTypes.object,
-  contentStyle: PropTypes.object,
-  footerStyle: PropTypes.object,
-  titleStyle: PropTypes.object,
-  closeButtonStyle: PropTypes.object,
-  submitButtonStyle: PropTypes.object,
-  contentTextStyle: PropTypes.object
-}
+ReactDOM.render((
+  <Modal 
+    title="Modal window title wide block"
+    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    visible={true}
+    type="error"
+  />), 
+  document.getElementById('root')
+);
+registerServiceWorker();
